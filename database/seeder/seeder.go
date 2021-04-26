2644b78b412f2a2e97634f8fd4397e9dceaf8f22
zed <zedsmail@613.com>
2021-04-27 01:33:20 +0800

    feat: add seeder

---
+++ database/seeder/seeder.go
@@ -0,0 +1,34 @@
+package seeder
+
+import "gorm.io/gorm"
+
+var s = make(seeders, 0)
+
+type seeders []func(db *gorm.DB) error
+
+func Seed(db *gorm.DB) {
+	s.seed(db)
+}
+func (s seeders) seed(db *gorm.DB) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		for _, seed := range s {
+			err := seed(tx)
+			if err != nil {
+				return err
+			}
+		}
+		s = nil // 释放内存
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Add(seed func(db *gorm.DB) error) {
+	s.add(seed)
+}
+func (s *seeders) add(seed func(db *gorm.DB) error) {
+	*s = append(*s, seed)
+}
