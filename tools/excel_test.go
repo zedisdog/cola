35bd2659d25843ab50185751e17dfed5354c25ec
zed <zedsmail@163.com>
2021-05-04 00:43:39 +0800

    feat: gen excel col index

---
+++ tools/excel_test.go
@@ -0,0 +1,34 @@
+package tools
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestCol(t *testing.T) {
+	num := 300
+	s := make([]string, 0, num/26)
+	for num != 0 {
+		tmp := num % 26
+		num /= 26
+
+		//此处略微关键，当为0时，其实是26，也就是Z，
+		//而且当你将0调整为26后，需要从数字中去除26代表的这个数
+		if tmp == 0 {
+			tmp = 26
+			num -= 1
+		}
+		s = append(s, fmt.Sprintf("%c", 'A'+tmp-1))
+	}
+
+	for i := 0; i < len(s)/2; i++ {
+		temp := s[i]
+		s[i] = s[len(s)-1-i]
+		s[len(s)-1-i] = temp
+	}
+
+	if strings.Join(s, "") != "KN" {
+		t.Fatal("error")
+	}
+}
