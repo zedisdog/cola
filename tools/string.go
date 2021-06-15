0382ed67b244da7395d35be94856d97a3bff9f10
zed <zedsmail@163.com>
2021-06-15 11:39:21 +0800

    feat: add help function

--- tools/string.go
+++ tools/string.go
@@ -1,9 +1,18 @@
 package tools
 
-import "regexp"
+import (
+	"regexp"
+)
 
 func IsMobile(str string) bool {
 	reg := `^1[3456789]\d{9}$`
 	r, _ := regexp.Compile(reg)
 	return r.MatchString(str)
+}
+
+func IsTrue(str string) bool {
+	if str != "" && str != "0" && str != "false" {
+		return true
+	}
+	return false
 }
