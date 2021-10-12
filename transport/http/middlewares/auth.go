be84df655604427d3576fbef4ce7a09aae171f77
zed <zedsmail@163.com>
2021-10-12 20:27:16 +0800

    refactor: optimization of auth middleware generator

--- transport/http/middlewares/auth.go
+++ transport/http/middlewares/auth.go
@@ -9,53 +9,48 @@ import (
 	"strings"
 )
 
+//GenAuthMiddleware generate an auth middleware
+//	claims: the customer Claims object
+//	key: the key for validate sign
+//	isUserExists: a function to determine if account is exists
 func GenAuthMiddleware(claims jwt.Claims, key string, isUserExists func(id interface{}) bool) func(*gin.Context) {
 	return func(c *gin.Context) {
+		var token string
 		if c.Request.Header.Get("Authorization") != "" {
 			arr := strings.Split(c.Request.Header.Get("Authorization"), " ")
 			if len(arr) < 2 {
 				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问1"})
 				return
 			}
-			token := arr[1]
-			err := auth.Parse(token, []byte(key), claims)
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问2"})
-				return
-			}
-
-			cv := reflect.ValueOf(claims)
-			id := cv.Elem().FieldByName("Id").String()
-			if !isUserExists(id) {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问3"})
-				return
-			}
-			c.Set("id", id)
-
-			roleField := cv.Elem().FieldByName("Role")
-			if roleField.IsValid() && roleField.String() != "" {
-				c.Set("role", roleField.String())
-			}
+			token = arr[1]
 		} else if c.Query("token") != "" {
-			err := auth.Parse(c.Query("token"), []byte(key), claims)
-			if err != nil {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问2"})
-				return
-			}
-
-			cv := reflect.ValueOf(claims)
-			id := cv.Elem().FieldByName("Id").String()
-			if !isUserExists(id) {
-				c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问3"})
-				return
-			}
-			c.Set("id", id)
-
-			roleField := cv.Elem().FieldByName("Role")
-			if roleField.IsValid() && roleField.String() != "" {
-				c.Set("role", roleField.String())
-			}
+			token = c.Query("token")
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问2"})
+			return
 		}
+
+		err := auth.Parse(token, []byte(key), claims)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问3"})
+			return
+		}
+
+		valueOfClaims := reflect.ValueOf(claims)
+
+		// if claims has property 'Role', set it into context
+		roleField := valueOfClaims.Elem().FieldByName("Role")
+		if roleField.IsValid() && roleField.String() != "" {
+			c.Set("role", roleField.String())
+		}
+
+		// use jti to keep the identification of account, jti in jwt.StandardClaims is Id
+		id := valueOfClaims.Elem().FieldByName("Id").String()
+		if !isUserExists(id) {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问4"})
+			return
+		}
+		c.Set("id", id)
 
 		c.Next()
 	}
