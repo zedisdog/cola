29ddfaa8ead73203ae11d434038566fac415576a
zed <zedsmail@163.com>
2021-10-26 16:42:42 +0800

    --

--- tools/json.go
+++ tools/json.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-type cTime struct {
+type CTime struct {
 	time.Time
 }
 
-func (c *cTime) UnmarshalJSON(b []byte) (err error) {
+func (c *CTime) UnmarshalJSON(b []byte) (err error) {
 	b = bytes.Trim(b, "\"")
 	zone := time.FixedZone("CST", int((8 * time.Hour).Seconds()))
 	tmp, err := time.ParseInLocation("2006-01-02", string(b), zone)
 	if err != nil {
 		return
 	}
-	*c = cTime{tmp}
+	*c = CTime{tmp}
 	return
 }
