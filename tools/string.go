a47581df627df3e149c97058c4a7b641d9fb8670
zed <zedsmail@163.com>
2021-11-14 12:09:30 +0800

    feat: add RandNumeric

--- tools/string.go
+++ tools/string.go
@@ -3,6 +3,7 @@ package tools
 import (
 	"math/rand"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -27,4 +28,17 @@ func RandString(len int) string {
 		bytes[i] = byte(b)
 	}
 	return string(bytes)
+}
+
+func RandNumeric(len int) string {
+	numeric := [10]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	var sb strings.Builder
+
+	for i := 0; i < len; i++ {
+		sb.WriteByte(numeric[r.Intn(10)])
+	}
+
+	return sb.String()
 }
