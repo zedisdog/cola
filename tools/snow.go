12dafad1d81a94dbf45adcf6dec3509e9c5013e9
zed <zedsmail@163.com>
2021-11-14 11:32:03 +0800

    feat: add config methods for GetSnow

--- tools/snow.go
+++ tools/snow.go
@@ -3,14 +3,37 @@ package tools
 import (
 	"github.com/sony/sonyflake"
 	"sync"
+	"time"
 )
 
 var once sync.Once
 var snow *sonyflake.Sonyflake
 
-func GetSnow() *sonyflake.Sonyflake {
+func WithStartTime(t time.Time) func(*sonyflake.Settings) {
+	return func(settings *sonyflake.Settings) {
+		settings.StartTime = t
+	}
+}
+
+func WithMachineIDFunc(f func() (uint16, error)) func(*sonyflake.Settings) {
+	return func(settings *sonyflake.Settings) {
+		settings.MachineID = f
+	}
+}
+
+func WithCheckMachineIDFunc(f func(uint16) bool) func(*sonyflake.Settings) {
+	return func(settings *sonyflake.Settings) {
+		settings.CheckMachineID = f
+	}
+}
+
+func GetSnow(setters ...func(*sonyflake.Settings)) *sonyflake.Sonyflake {
+	setting := sonyflake.Settings{}
+	for _, setter := range setters {
+		setter(&setting)
+	}
 	once.Do(func() {
-		snow = sonyflake.NewSonyflake(sonyflake.Settings{}) //todo: 配置
+		snow = sonyflake.NewSonyflake(setting)
 	})
 	return snow
 }
